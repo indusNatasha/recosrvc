34d83dd757de771dc45e4b91261eca3973d3d272
indusNatasha <sheets27022024@gmail.com>
2026-06-27 00:41:10 +0700

    Add files via upload

---
+++ initSecure.js
@@ -0,0 +1,37 @@
+import { initBiometric, authenticateUser } from "./biometric.js";
+
+
+export function runSecureLogin(statusTextEl, retryBtnEl) {
+    
+    statusTextEl.innerText = "Проверка сканера...";
+    retryBtnEl.style.display = "none";
+
+    // Шаг 1: Инициализируем биометрию
+    initBiometric(
+        // Колбэк, если всё готово (onReady)
+        function() {
+            statusTextEl.innerText = "Приложите палец или посмотрите в камеру...";
+            
+            // Шаг 2: Вызываем сканер
+            authenticateUser(
+                // Колбэк при успешном сканировании (onSuccess)
+                function(bioToken) {
+                    statusTextEl.innerText = "Успешно! Доступ разрешен.";
+                    console.log("Получен токен биометрии:", bioToken);
+                    
+                    // Прячем экран загрузки
+                    document.getElementById('loading-screen').style.display = 'none';
+                },
+                // Колбэк, если юзер приложил чужой палец или закрыл окно (onError)
+                function(err) {
+                    statusTextEl.innerText = err;
+                    retryBtnEl.style.display = "block";
+                }
+            );
+        },
+        // Колбэк, если на телефоне вообще нет биометрии (onError)
+        function(err) {
+            statusTextEl.innerText = "Ошибка: " + err;
+        }
+    );
+}
